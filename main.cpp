4dc30ff2efc279b60359ad945bb2a3cb3681070e
Euclid Skyline <euclid.skyline@gmail.com>
2025-07-29 02:22:51 +0300

    Add logical device and queue management in Vulkan setup
    - Introduced `createLogicalDevice()` method for creating the Vulkan logical device and obtaining the graphics queue.
    - Updated `initVulkan()` to include a call to `createLogicalDevice()`.
    - Enhanced `cleanup()` to properly destroy the logical device.

--- main.cpp
+++ main.cpp
@@ -61,6 +61,8 @@ private:
     VkSurfaceKHR surface = VK_NULL_HANDLE;
     VkDebugUtilsMessengerEXT debugMessenger = VK_NULL_HANDLE;
     VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    VkDevice device;
+    VkQueue graphicsQueue;
 
     void initWindow() {
         // --- Initialize SDL3 ---
@@ -99,6 +101,7 @@ private:
         setupDebugMessenger();
         createSurface();
         pickPhysicalDevice();
+        createLogicalDevice();
     }
 
     void mainLoop() {
@@ -117,7 +120,7 @@ private:
 
     void cleanup() {
 
-
+        vkDestroyDevice(device, nullptr);
         if (enableValidationLayers) {
             DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
         }
@@ -226,6 +229,40 @@ private:
         if (physicalDevice == VK_NULL_HANDLE) {
             throw std::runtime_error("failed to find a suitable GPU!");
         }
+    }
+
+    void createLogicalDevice() {
+        QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+        VkDeviceQueueCreateInfo queueCreateInfo{};
+        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+        queueCreateInfo.queueCount = 1;
+        float queuePriority = 1.0f;
+        queueCreateInfo.pQueuePriorities = &queuePriority;
+        VkPhysicalDeviceFeatures deviceFeatures{};
+
+        VkDeviceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+		createInfo.pQueueCreateInfos = &queueCreateInfo; // 1. queue create info
+        createInfo.queueCreateInfoCount = 1; 
+
+		createInfo.pEnabledFeatures = &deviceFeatures; // 2. physical device features
+		createInfo.enabledExtensionCount = 0; // 3. enabled extensions count
+
+        if (enableValidationLayers) {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        }
+        else {
+            createInfo.enabledLayerCount = 0;
+        }
+
+        if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create logical device!");
+        }
+
+        vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
     }
 
     std::vector<const char*> getRequiredExtensions() {
