9940548db58bb25d97bd518fa6214b37b96b7e0d
Euclid Skyline <euclid.skyline@gmail.com>
2025-08-06 21:57:37 +0300

    Update vertex colors and camera view in Vulkan setup
    - Modified vertex colors and order in `UniformBufferObject`.
    - Enabled non-solid fill mode in Vulkan device features.
    - Updated rasterization state with comments on polygon mode.
    - Adjusted camera view for top perspective; added alternative view.

--- main.cpp
+++ main.cpp
@@ -105,13 +105,13 @@ struct UniformBufferObject {
 };
 
 const std::vector<Vertex> vertices = {
-    {{0.0f, 0.0f}, {1.0f, 0.0f, 0.0f}},
-    {{0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}},
-    {{0.25f, 0.433f}, {0.0f, 0.0f, 1.0f}},
-    {{-0.25f, 0.433f}, {1.0f, 1.0f, 1.0f}},
-    {{-0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}},
+    {{0.0f, 0.0f},      {1.0f, 1.0f, 1.0f}},
+    {{0.5f, 0.0f},      {1.0f, 0.0f, 0.0f}},
+    {{0.25f, 0.433f},   {0.0f, 1.0f, 0.0f}},
+    {{-0.25f, 0.433f},  {0.0f, 0.0f, 1.0f}},
+    {{-0.5f, 0.0f},     {1.0f, 0.0f, 0.0f}},
     {{-0.25f, -0.433f}, {0.0f, 1.0f, 0.0f}},
-    {{0.25f, -0.433f}, {1.0f, 0.0f, 0.0f}}
+    {{0.25f, -0.433f},  {0.0f, 0.0f, 1.0f}}
 };
 
 //const std::vector<Vertex> vertices = {
@@ -463,6 +463,7 @@ private:
         }
 
         VkPhysicalDeviceFeatures deviceFeatures{};
+		deviceFeatures.fillModeNonSolid = VK_TRUE; // Enable non-solid fill mode for triangle rendering
 
         VkDeviceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
@@ -730,7 +731,7 @@ private:
         rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
         rasterizer.depthClampEnable = VK_FALSE;
 		rasterizer.rasterizerDiscardEnable = VK_FALSE;
-		rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
+		rasterizer.polygonMode = VK_POLYGON_MODE_FILL; // Alternative use VK_POLYGON_MODE_LINE for wireframe rendering
         rasterizer.lineWidth = 1.0f;
         rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
         rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
@@ -1146,7 +1147,16 @@ private:
 
         UniformBufferObject ubo{};
         ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-        ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        //ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        // Look into the z-axis (top view). You may need adjust the rasterization state culling mode and front face.
+        ubo.view = glm::lookAt(glm::vec3(0.0f, 0.0f, 2.0f),  // Camera at +Z
+            glm::vec3(0.0f, 0.0f, 0.0f),  // Looking at origin
+            glm::vec3(0.0f, 1.0f, 0.0f)); // Y is up
+		// Look into the z-axis (bottom view). You may need adjust the rasterization state culling mode and front face.
+        //ubo.view = glm::lookAt(glm::vec3(0.0f, 0.0f, -2.0f), // Camera at -Z
+        //    glm::vec3(0.0f, 0.0f, 0.0f),  // Looking at origin
+        //    glm::vec3(0.0f, 1.0f, 0.0f)); // Y is up
+
         ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float)swapChainExtent.height, 0.1f, 10.0f);
 		ubo.proj[1][1] *= -1; // Invert the Y axis for Vulkan
 
